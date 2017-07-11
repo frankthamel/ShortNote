bd57c238bb95fe717ca45060b3a694baed97a8b5
frank thamel <w.frankthamel@gmail.com>
2017-07-11 09:32:14 +0530

    Initial Commit

---
+++ Short Note/Short Note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Short Note
+//
+//  Created by frank thamel on 7/11/17.
+//  Copyright © 2017 Crowderia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
